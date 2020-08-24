31474d12e04032da090a274f02cc24c841551833
Pablo Toledo Margalef <pabloatm980@gmail.com>
2020-08-24 10:53:57 -0300

    Fix declaration of constants

--- src/card.h
+++ src/card.h
@@ -1,3 +1,6 @@
+#ifndef CARD_H_
+#define CARD_H_
+
 #define CARD_QUANTITY 40
 #define SUIT_QUANTITY 4
 #define VALUES_QUANTITY 10
@@ -21,7 +24,7 @@ typedef struct {
 
 typedef card_t deck_t[CARD_QUANTITY];
 
-card_t card_scale[SCALE_LENGTH] = {
+static const card_t card_scale[SCALE_LENGTH] = {
     {.suit=Espada, .value=1},
     {.suit=Basto, .value=1},
     {.suit=Espada, .value=7},
@@ -38,10 +41,13 @@ card_t card_scale[SCALE_LENGTH] = {
     {.suit=Any, .value=4},
 };
 
-card_value_t possible_values[VALUES_QUANTITY] = {1,2,3,4,5,6,7,10,11,12};
-card_suit_t possible_suits[SUIT_QUANTITY] = {Basto, Copa, Espada, Oro};
+static const card_value_t possible_values[VALUES_QUANTITY] = {1,2,3,4,5,6,7,10,11,12};
+static const card_suit_t possible_suits[SUIT_QUANTITY] = {Basto, Copa, Espada, Oro};
 
 int initialize_deck(deck_t deck);
 char *suit_to_string(card_suit_t suit);
 int card_rank(card_t card);
 int cardcmp(card_t a, card_t b);
+
+
+#endif // CARD_H_
